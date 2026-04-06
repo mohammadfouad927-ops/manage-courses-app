809c9254f9b87b3297e597d369eca68e3640b8c4
mohammadfouad927-ops <mohammadfouad927@gmail.com>
2026-04-06 19:28:20 +0200

    feat: add student routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CoursesController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,3 +13,5 @@ Route::get('/home', function(){
 });
 
 Route::resource('courses',CoursesController::class);
+
+Route::resource('students', StudentController::class);
